43b24e54ee6886ec6cdde3f440fb985c8a0cf172
Websyspro <cpd.emersontsa@gmail.com>
2026-09-25 17:23:49 -0300

    Release 1.0.53

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-defined( "BASE_DIR" ) || define(
-  "BASE_DIR", __DIR__ . DIRECTORY_SEPARATOR
-);
-
-require_once BASE_DIR . "vendor/autoload.php";
-require_once BASE_DIR . "main.php";
--- main.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Websyspro\Server\Request;
-use Websyspro\Server\WorkerServer;
-
-$ws = new WorkerServer(); 
-$ws->get( "/test/:id", fn( Request $request ) => $request->body );
-$ws->start();
